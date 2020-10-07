46636321bb8a7f45687e409cf5bd28b68ae15e57
Arphik <sebo44@gmail.com>
2020-10-07 14:17:41 +0200

    Build.

---
+++ build/precache-manifest.0535aee47ac5be15e55aab5fc97f7a04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ec9891819b97c966a7c975b06a83c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5dd22d3ffa598b7eedda",
+    "url": "./static/css/main.3fe5ae31.chunk.css"
+  },
+  {
+    "revision": "8642eac550d92e5e8339",
+    "url": "./static/js/2.01df07dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.01df07dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd22d3ffa598b7eedda",
+    "url": "./static/js/main.b08e788d.chunk.js"
+  },
+  {
+    "revision": "518f5b07cb9a7cddde44",
+    "url": "./static/js/runtime-main.16c17ea7.js"
+  },
+  {
+    "revision": "98162c323f3ecb25914cf79f1f1bcd93",
+    "url": "./static/media/background.98162c32.png"
+  },
+  {
+    "revision": "6d9937925c1f37f651e2029863a5054f",
+    "url": "./static/media/image.6d993792.png"
+  }
+]);
